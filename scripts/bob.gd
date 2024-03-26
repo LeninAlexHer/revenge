99819b6e0b423c1de96592da900c201ff664966b
Carlos Penagos <cpenagos@ihungo.com>
2024-03-25 21:42:04 -0500

    add new features for player walk and idle

--- scripts/bob.gd
+++ scripts/bob.gd
@@ -5,6 +5,8 @@ const MAX_SPEED = 80
 const FRICTION = 500
 
 @onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(_delta):
 	var input_vector = Vector2.ZERO
@@ -13,10 +15,12 @@ func _physics_process(_delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/walk/blend_position", input_vector)
+		animationState.travel("walk")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * _delta)
-		animationPlayer.play("Walk")
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * _delta)
-		animationPlayer.play("Idle")
 	
-	move_and_collide(velocity * _delta)
+	move_and_slide()
